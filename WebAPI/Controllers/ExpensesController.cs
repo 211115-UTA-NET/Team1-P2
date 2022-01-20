3603fc4b082a01e7ad76a9d9a77b65a045600442
Stefan Schweers <schweers.stefan@yahoo.com>
2022-01-19 20:55:48 -0500

    Commpleted initial Http request files

--- WebAPI/Controllers/ExpensesController.cs
+++ WebAPI/Controllers/ExpensesController.cs
@@ -23,16 +23,16 @@ namespace WebAPI.Controllers
             string connect = _configuration.GetSection("ConnectionString").GetSection("BankedDB").Value;
             using SqlConnection connection = new(connect);
 
-            return ExpensesInputService.GetExpense(userId, connection);
+            return ExpensesService.GetExpense(userId, connection);
 
         }
         //Need to create list value to add as input parameter containing all data required for expense input
         [HttpPost]
-        public async void AddExpense(List<Expenses_Dto> expense)
+        public async void PostExpense(List<Expenses_Dto> expense)
         {
             string connect = _configuration.GetSection("ConnectionString").GetSection("PrintShopDB").Value;
             await using SqlConnection connection = new(connect);
-            ExpensesInputService.InputExpense(expense, connection);
+            ExpensesService.InputExpense(expense, connection);
         }
     }
 }
---
+++ WebAPI/Services/SavingsService.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Data.SqlClient;
+using WebAPI.Models;
+
+namespace WebAPI.Logic
+{
+    public class SavingsService
+    {
+        static List<Savings_Dto> expenses { get; }
+        static SavingsService()
+        {
+            expenses = new List<Savings_Dto>
+            {
+                //if you would like to manually add a custom item
+                //new Catalog {ItemID = 18, ItemName = "SuperPrint 1000", Location = "North St.", Price = 1000, quantity = 10}
+            };
+        }
+
+        //Get Individual Userinfo based on username input
+        public static List<Savings_Dto> GetSavings(int userId, SqlConnection connection)
+        {
+            List<Savings_Dto> currentItem = new();
+
+            string sql = $"--ENTER GET COMMAND--"; //use userId to query for expense items
+
+            connection.Open();
+            using SqlCommand command = new SqlCommand(sql, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                //set items in table to fields in the Dto to send back on the Get command
+                var item = new Savings_Dto()
+                {
+                    //Id = (int)reader[0],
+                    //UserPassId = username,
+                    //Password = reader[2].ToString(),
+                    //FirstName = reader[3].ToString(),
+                    //LastName = reader[4].ToString(),
+                };
+                currentItem.Add(item);
+            }
+            reader.Close();
+            connection.Close();
+            return currentItem;
+        }
+
+
+        //Insert Expense to Database
+        public static void InputSavings(List<Savings_Dto> expense, SqlConnection connection)
+        {
+            string sql = $"--ENTER INSERT COMMAND--"; //expense.Id(0) for single object id input
+
+            connection.Open();
+            using SqlCommand command = new(sql, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
