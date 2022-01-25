674d670ea7bee852e97b3297eee0a49fc8604413
Stefan Schweers <schweers.stefan@yahoo.com>
2022-01-25 16:08:57 -0500

    updated calculations to actually add values

--- WebAPI/Logic/GraphCalculations.cs
+++ WebAPI/Logic/GraphCalculations.cs
@@ -16,66 +16,72 @@ namespace WebAPI.Logic
 
         public decimal[] CalculateTotal(List<Expenses_Dto> Expenses, List<Income_Dto> Income, List<Loans_Dto> Loans, List<Savings_Dto> Savings)
         {
-            //Lists to collect data. Savings goal and Dates calculated in client
-            List<decimal> TotalList = new();
+            //Array to collect data. Savings goal and Dates calculated in client. 104 Values is 2 years
+            decimal[] List = new decimal[104];
 
             //Foreach loops to collect info, adding and subtracting from total as needed.
+            //while loops to iterate through each array item
             int i = 0;
             foreach (var item in Expenses)
             {
-                int T = CalculateTime(item.ExpenseFrequency);
-                TotalList[i] -= item.ExpenseAmount * T;
-                i++;
+                while(i <= 104)
+                {
+                    int T = CalculateTime(item.ExpenseFrequency);
+                    List[i] -= item.ExpenseAmount * T;
+                    i++;
+                }
+                i = 0;
             }
 
 
-            i = 0;
             foreach (var item in Income)
             {
-                int T = CalculateTime(item.PaySchedule);
-                TotalList[i] += item.IncomeAmount * T;
-                i++;
+                while(i <= 104)
+                {
+                    int T = CalculateTime(item.PaySchedule);
+                    List[i] += item.IncomeAmount * T;
+                    i++;
+                }
+                i = 0;
             }
 
 
             //Savings and Loans only opperate monthly and have interest. Stopping loans at end of items or loan zero'd out
             i = 3;
-            int Length = TotalList.Count;
             foreach (var item in Loans)
             {
-                while(Length >= 4 || item.LoanAmount > 0)
+                while(i <= 104)
                 {
-                    decimal interest = ((item.LoanAmount * ((decimal)item.LoanInterest) / 100) - item.MonthlyPayments) / 4;
+                    while(item.LoanAmount > 0 || i >= 104)
+                    {
+                        decimal interest = ((item.LoanAmount * ((decimal)item.LoanInterest) / 100) - item.MonthlyPayments) / 4;
 
-                    TotalList[i] -= interest;
+                        List[i] -= interest;
 
-                    item.LoanAmount += interest;
-                    i += 4;
-                    Length -= 4;
-                } 
+                        item.LoanAmount += interest;
+                        i += 4;
+                    } 
+                }
+                i = 0;
             }
 
 
             i = 3;
-            Length = TotalList.Count;
             foreach (var item in Savings)
             {
-                while (Length >= 4)
+                while(i <= 104)
                 {
                     decimal interest = ((item.SavingsAmount * ((decimal)item.SavingsInterest) / 100) + item.SavingsAddedMonthly) / 4;
 
-                    TotalList[i] += interest;
+                    List[i] += interest;
 
                     item.SavingsAmount += interest;
                     i += 4;
-                    Length -= 4;
                 }
+
             }
 
-            //convert lists to arrays
-            decimal[] Total = TotalList.ToArray();
-
-            return Total;
+            return List;
             
 
 
