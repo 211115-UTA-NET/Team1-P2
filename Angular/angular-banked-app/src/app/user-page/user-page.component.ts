cfa0c09c04e56cbb26c69eb5cc2eeda1caa0f1e5
Kyler Dennis <KylerGDennis@gmail.com>
2022-01-26 12:41:28 -0500

    Added user side errors for GET/POST methods

--- Angular/angular-banked-app/src/app/user-page/user-page.component.ts
+++ Angular/angular-banked-app/src/app/user-page/user-page.component.ts
@@ -15,7 +15,8 @@ import { Savings } from '../Savings';
 
 export class UserPageComponent implements OnInit {
 
-  userId: string | any = localStorage.getItem("userid");
+  //userId: string | any = localStorage.getItem("userid");
+  userId: number = 1;
   createExpense: boolean = false;
   createLoan: boolean = false;
   createSavings: boolean = false;
@@ -26,6 +27,9 @@ export class UserPageComponent implements OnInit {
   expenses: Expense[] = [];
   tempExpense: Expense[] = [];
   newExpense!: Expense;
+  expenseGetError: boolean = false;
+  expensePostError: boolean = false;
+  expensePostErrorForm: boolean = false;
   @Input() expenseId!: number;
   @Input() expenseAmount!: number;
   @Input() expenseFrequency!: number;
@@ -35,6 +39,9 @@ export class UserPageComponent implements OnInit {
   incomes: Income[] = [];
   tempIncome: Income[] = [];
   newIncome!: Income;
+  incomeGetError: boolean = false;
+  incomePostError: boolean = false;
+  incomePostErrorForm: boolean = false;
   @Input() incomeId!: number;
   @Input() incomeAmount!: number;
   @Input() paySchedule!: number;
@@ -42,6 +49,9 @@ export class UserPageComponent implements OnInit {
   loans: Loan[] = [];
   tempLoan: Loan[] = [];
   newLoan!: Loan;
+  loanGetError: boolean = false;
+  loanPostError: boolean = false;
+  loanPostErrorForm: boolean = false;
   @Input() loanName!: string;
   @Input() loanAmount!: number;
   @Input() loanInterest!: number;
@@ -50,6 +60,9 @@ export class UserPageComponent implements OnInit {
   savings: Savings[] = [];
   tempSavings: Savings[] = [];
   newSavings!: Savings;
+  savingsGetError: boolean = false;
+  savingsPostError: boolean = false;
+  savingsPostErrorForm: boolean = false;
   @Input() savingsName!: string;
   @Input() savingsAmount!: number;
   @Input() savingsInterest!: number;
@@ -62,6 +75,14 @@ export class UserPageComponent implements OnInit {
 
   clearUser(): void {
     localStorage.clear();
+  }
+
+  clearGetErrors()
+  {
+    this.loanGetError = false;
+    this.expenseGetError = false;
+    this.savingsGetError = false;
+    this.incomeGetError = false;
   }
 
   showIncome(): void{
@@ -90,12 +111,28 @@ export class UserPageComponent implements OnInit {
     this.createLoan = true;
     this.createSavings = false;
     this.createIncome = false;
+    this.loanPostErrorForm = false;
+    this.loanPostError = false;
   }
 
   getExpenses(): void
   {
+    this.clearGetErrors();
+
+    try
+    {
       this.bankedService.getExpenses(this.userId)
       .subscribe(retObject => this.CheckExpenseApi(retObject));
+    }
+    catch //not catching
+    {
+      this.expenseGetError = true;
+    }
+
+    if(this.expenses.length == 0)
+    {
+      this.expenseGetError = true;
+    }
   }
   CheckExpenseApi(retObject: Expense[]): void {
     this.incomes = [];
@@ -106,6 +143,8 @@ export class UserPageComponent implements OnInit {
 
   async postExpense(
     ){
+      this.expensePostErrorForm = false;
+      this.expensePostError = false;
       this.expenses = [];
       this.tempExpense = [];
       this.newExpense = {} as Expense;
@@ -116,18 +155,43 @@ export class UserPageComponent implements OnInit {
       this.newExpense.expenseFrequency = this.expenseFrequency;
       this.newExpense.expenseEnding = this.expenseDate;
       this.newExpense.priority = this.expenseSeverity;
-      if (!this.userId || this.expenseId == -1 || this.expenseAmount == -1 || this.expenseFrequency == -1 || !this.expenseDate)
-      { return; }
+      if (!this.userId || !this.expenseId || !this.expenseAmount || !this.expenseFrequency || !this.expenseDate)
+      { 
+        this.expensePostErrorForm = true;
+        return; 
+      }
 
       this.tempExpense.push(this.newExpense);
 
-      this.expenses = await this.bankedService.postExpense(this.tempExpense);
+      try
+      {
+        this.expenses = await this.bankedService.postExpense(this.tempExpense);
+      }
+      catch
+      {
+        this.expensePostError = true;
+      }
     }
 
     getIncomes(): void
   {
+    this.clearGetErrors();
+
+    try
+    {
       this.bankedService.getIncomes(this.userId)
       .subscribe(retObject => this.CheckIncomeApi(retObject));
+    }
+    catch //not catching
+    {
+      this.incomeGetError = true;
+    }
+
+    if(this.incomes.length == 0)
+    {
+      this.incomeGetError = true;
+    }
+      
   }
   CheckIncomeApi(retObject: Income[]): void {
     this.expenses = [];
@@ -138,6 +202,8 @@ export class UserPageComponent implements OnInit {
 
   async postIncome(
     ){
+      this.incomePostErrorForm = false;
+      this.incomePostError = false;
       this.incomes = [];
       this.tempIncome = [];
       this.newIncome = {} as Income;
@@ -146,18 +212,43 @@ export class UserPageComponent implements OnInit {
       this.newIncome.incomeOptions = this.incomeId;
       this.newIncome.incomeAmount = this.incomeAmount;
       this.newIncome.paySchedule = this.paySchedule;
-      if (!this.userId || this.incomeId == -1 || this.incomeAmount == -1 || this.paySchedule == -1)
-      { return; }
+      if (!this.userId || !this.incomeId || !this.incomeAmount || !this.paySchedule)
+      { 
+        this.incomePostErrorForm = true;
+        return; 
+      }
 
       this.tempIncome.push(this.newIncome);
 
-      this.incomes = await this.bankedService.postIncome(this.tempIncome);
+      try
+      {
+        this.incomes = await this.bankedService.postIncome(this.tempIncome);
+      }
+      catch
+      {
+        this.incomePostError = true;
+      }
     }
 
     getLoans(): void
   {
+    this.clearGetErrors();
+
+    try
+    {
       this.bankedService.getLoans(this.userId)
-        .subscribe(retObject => this.CheckLoanApi(retObject));
+      .subscribe(retObject => this.CheckLoanApi(retObject));
+    }
+    catch //not catching
+    {
+      this.loanGetError = true;
+    }
+
+    if(this.loans.length == 0)
+    {
+      this.loanGetError = true;
+    }
+      
   }
   CheckLoanApi(retObject: Loan[]): void {
     this.incomes = [];
@@ -168,6 +259,8 @@ export class UserPageComponent implements OnInit {
 
   async postLoan(
     ){
+      this.loanPostErrorForm = false;
+      this.loanPostError = false;
       this.loans = [];
       this.tempLoan = [];
       this.newLoan = {} as Loan;
@@ -177,18 +270,42 @@ export class UserPageComponent implements OnInit {
       this.newLoan.loanAmount = this.loanAmount;
       this.newLoan.loanInterest = this.loanInterest;
       this.newLoan.monthlyPayments = this.monthlyPayments;
-      if (!this.userId || !this.loanName || this.loanAmount == -1 || this.loanInterest == -1 || this.monthlyPayments == -1)
-      { return; }
+      if (!this.userId || !this.loanName || !this.loanAmount || !this.loanInterest || !this.monthlyPayments)
+      {
+        this.loanPostErrorForm = true;
+        return;
+      }
 
       this.tempLoan.push(this.newLoan);
 
-      this.loans = await this.bankedService.postLoan(this.tempLoan);
+      try
+      {
+        this.loans = await this.bankedService.postLoan(this.tempLoan);
+      }
+      catch
+      {
+        this.loanPostError = true;
+      }
     }
 
     getSavings(): void
   {
+    this.clearGetErrors();
+
+    try
+    {
       this.bankedService.getSavings(this.userId)
       .subscribe(retObject => this.CheckSavingsApi(retObject));
+    }
+    catch //not catching
+    {
+      this.savingsGetError = true;
+    }
+
+    if(this.savings.length == 0)
+    {
+      this.savingsGetError = true;
+    }
   }
   CheckSavingsApi(retObject: Savings[]): void {
     this.expenses = [];
@@ -199,6 +316,8 @@ export class UserPageComponent implements OnInit {
 
   async postSavings(
     ){
+      this.savingsPostErrorForm = false;
+      this.savingsPostError = false;
       this.savings = [];
       this.tempSavings = [];
       this.newSavings = {} as Savings;
@@ -208,12 +327,22 @@ export class UserPageComponent implements OnInit {
       this.newSavings.savingsAmount = this.loanAmount;
       this.newSavings.savingsInterest = this.loanInterest;
       this.newSavings.savingsAddedMonthly = this.monthlyPayments;
-      if (!this.userId || !this.savingsName || this.savingsAmount == -1 || this.savingsInterest == -1 || this.addedMonthly == -1)
-      { return; }
+      if (!this.userId || !this.savingsName || !this.savingsAmount || !this.savingsInterest || !this.addedMonthly)
+      { 
+        this.savingsPostErrorForm = true;
+        return; 
+      }
 
       this.tempSavings.push(this.newSavings);
 
-      this.savings = await this.bankedService.postSavings(this.tempSavings);
+      try
+      {
+        this.savings = await this.bankedService.postSavings(this.tempSavings);
+      }
+      catch
+      {
+        this.savingsPostError = true;
+      }
     }
 
     getGraphData(): void
