0f328748adc00288ccc2c8522f52a91ec7a0bdad
Stefan S <94373157+SSchweers@users.noreply.github.com>
2022-01-27 20:11:47 -0500

    Update ExpensesService.cs

--- WebAPI/Services/ExpensesService.cs
+++ WebAPI/Services/ExpensesService.cs
@@ -90,7 +90,9 @@ namespace WebAPI.Logic
       await reader.ReadAsync();      
       await connection.CloseAsync();
       if (_logger is  not null)
+      {
       _logger.LogInformation("executed select statement for Expenses of user id {userId}", userId);
+      }
       return currentItem;
     }
 
